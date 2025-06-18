0fb398254ecee9a349e63486519002b7f182f8fe
DeF4lt-Cap10N <captainakash1675@gmail.com>
2025-06-18 18:45:48 +0530

    add  backend

--- week-5/backend/routes/todo.js
+++ week-5/backend/routes/todo.js
@@ -12,11 +12,23 @@ router.get("/", (req, res) => {
 })
 
 
+router.get("/:userId", (req, res) => {
+    const {userId} = req.params;
+    const userTodo = todos.filter((todo)=>todo.userId===userId);
+    if(userTodo.length>0){
+        res.status(200).json(userTodo);
+    }
+    else{
+        res.status(404).send("Todo Not available");
+    }
+})
+
+
 router.post("/create", (req, res) => {
-    const data = req.body.data;
+    const {data, userId} = req.body;
     const id = uuidv4();
-    if (data) {
-        todos.push({ id, data });
+    if (data && userId) {
+        todos.push({ id, data , userId});
         return res.status(201).send("todo created");
     } else {
         return res.status(204).send("data is empty");
@@ -27,13 +39,18 @@ router.post("/create", (req, res) => {
 
 router.put("/update/:id", (req, res) => {
     const id = req.params.id;
+    const userId = req.body.userId;
     const index = todos.findIndex((e) => e.id === id);
 
-    if (index!==-1) {
+    if(todos[index].userId!==userId){
+        return res.status(401).send("Not Authorize access");
+    }
+
+    if (index !== -1) {
         const data = req.body.data;
-        todos[index] = {...todos[index], data}; 
+        todos[index] = { ...todos[index], data };
         return res.status(200).send("updates");
-    }else{
+    } else {
         return res.status(404).send("id not found");
     }
 })
@@ -41,12 +58,21 @@ router.put("/update/:id", (req, res) => {
 
 router.delete("/delete/:id", (req, res) => {
     const id = req.params.id;
-    const index = todos.findIndex((e)=>e.id===id);
+    const userId = req.body.userId;
+    // const index = todos.findIndex((e)=>e.id===id); 
+    const present = todos.find((e) => e.id === id);
+    const index = todos.indexOf(present);
 
-    if(index!==-1){
-        todos.splice(index , 1);
+     if(todos[index].userId!==userId){
+        return res.status(401).send("Not Authorize access");
+    }
+
+
+    if (present) {
+
+        todos.splice(index, 1);
         res.status(200).send("Deleted todo");
-    }else{
+    } else {
         return res.status(404).send("id not found");
     }
 })
--- week-5/backend/routes/user.js
+++ week-5/backend/routes/user.js
@@ -1,13 +1,52 @@
 //  start writing your code from here
 //  start writing your code from here
-const {Router} = require("express");
+const { Router } = require("express");
 const router = Router();
 
+const { v4: uuidv4 } = require("uuid");
 
-const user = {};
 
-router.get("/registion", (req, res)=>{
-    res.send("registion")
+const user = [];
+
+
+// just for devlopment show all user
+router.get("/show", (req, res) => {
+    if (user.length > 0) {
+        res.status(200).json(user);
+    }
+    else {
+        res.status(404).send("user empty");
+    }
+})
+
+router.post("/registration", (req, res) => {
+    const id = uuidv4();
+    const { userName, userPassword } = req.body;
+
+    const checkUser = user.find((e)=>e.userName===userName);
+    if(checkUser){
+       return res.status(406).send("user already regitered");
+    }
+
+    if (!userName || !userPassword) {
+        res.status(409).send("user data missing!");
+    } else {
+        user.push({ id, userName, userPassword });
+        res.status(201).send("user registered sucessfully");
+    }
+})
+
+
+router.post("/login", (req, res) => {
+    const { userName, userPassword } = req.body;
+    const foundUser = user.find((e) => e.userName === userName);
+
+    if (foundUser && foundUser.userPassword === userPassword) {
+        res.status(200).send(foundUser.id);
+    }
+    else {
+        res.status(404).send("Invalid user");
+    }
 })
 
 
